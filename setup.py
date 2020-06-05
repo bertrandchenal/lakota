964b101fe0a8d14c621f649573cf0046e0350ddc
Bertrand Chenal <bertrand@adimian.com>
2020-06-05 10:48:56 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+#import baltic
+
+long_description = '''
+Baltic organise reads and writes through a changelog inspired by
+Git (and by DVCSs in general). This changelog provides: historisation,
+concurrency control and ease of synchronisation across different
+storages.
+'''
+
+description = ('Versioned storage for data series')
+
+setup(name='baltic',
+      version='0.0',
+      description=description,
+      long_description=long_description,
+      author='Bertrand Chenal',
+      url='https://github.com/bertrandchenal/baltic',
+      license='MIT',
+      packages=['baltic'],
+      install_requires=['numpy', 'zarr'],
+      # py_modules=['baltic'],
+      entry_points={
+          'console_scripts': [
+              'baltic = baltic.cli:run',
+          ],
+      },
+  )
