707f963347ad22b4b9b91c03a4bf19acb3c23f7c
Bertrand Chenal <bertrand.chenal@engie.com>
2020-10-08 17:42:02 +0200

    Add s-expression parsing

---
+++ lakota/sexpr.py
@@ -0,0 +1,154 @@
+import operator
+from functools import partial, reduce
+
+import numpy
+from pyparsing import (
+    Forward,
+    Group,
+    ParseResults,
+    QuotedString,
+    Suppress,
+    Word,
+    ZeroOrMore,
+    alphanums,
+    oneOf,
+    pyparsing_common,
+)
+
+_getter_default = object()
+
+
+def getter(obj, field, default=_getter_default):
+    try:
+        return obj[field]
+    except KeyError:
+        pass
+    try:
+        return getattr(obj, field)
+    except AttributeError:
+        if default != _getter_default:
+            return default
+        raise
+
+
+def list_to_dict(items):
+    it = iter(items)
+    return dict(zip(it, it))
+
+
+class Token:
+    def __init__(self, value):
+        self.value = value[0] if len(value) == 1 else value
+
+    def __repr__(self):
+        return f"<{type(self).__name__} {self.value}>"
+
+
+class Identifier(Token):
+    def eval(self, env):
+        head, *tail = self.value.split(".")
+        res = getter(env, head)
+        for item in tail:
+            res = getter(res, item)
+        return res
+
+
+class Bool(Token):
+    def eval(self, env):
+        return self.value == "true"
+
+
+class Litteral(Token):
+    def eval(self, env):
+        return self.value
+
+
+class Operator(Token):
+    op_table = {
+        "+": partial(reduce, operator.add),
+        "-": partial(reduce, operator.sub),
+        "*": partial(reduce, operator.mul),
+        "/": partial(reduce, operator.truediv),
+        "and": partial(reduce, operator.and_),
+        "or": partial(reduce, operator.or_),
+        "<": partial(reduce, operator.lt),
+        "<=": partial(reduce, operator.le),
+        "=": partial(reduce, operator.eq),
+        "!=": partial(reduce, operator.ne),
+        ">=": partial(reduce, operator.ge),
+        ">": partial(reduce, operator.gt),
+        "~": lambda xs: all(not x for x in xs),
+        "in": lambda x: x[0] in x[1:],
+        "list": list,
+        "dict": list_to_dict,
+    }
+
+    def eval(self, tokens, env):
+        op = self.op_table[self.value]
+        return op(tokens)
+
+
+class AST:
+    def __init__(self, tokens):
+        self.tokens = tokens
+
+    @classmethod
+    def parse(cls, code):
+        tokens = sexp.parseString(code)[0]
+        return AST(tokens)
+
+    def eval_token(self, tk, env):
+        if isinstance(tk, Token):
+            value = tk.eval(env)
+        elif isinstance(tk, (list, ParseResults)):
+            value = AST(tk).eval(env)
+        else:
+            raise ValueError(f"unexpected item: {tk}")
+        return value
+
+    def eval(self, env=None):
+        env = env or {}
+        args = []
+        # Eval args
+        for a in self.tokens[1:]:
+            value = self.eval_token(a, env)
+            args.append(value)
+        # Eval operator
+        head = self.tokens[0]
+        if isinstance(head, Operator):
+            return head.eval(args, env)
+        elif isinstance(head, Identifier):
+            ident = head.value
+            fn = getter(env, ident, None)
+            if fn is None:
+                fn = getattr(numpy, ident, None)
+            if fn is None:
+                raise NameError(f"{ident} is not defined")
+            if not callable(fn):
+                raise TypeError(f"{ident} is not callable")
+            return fn(*args)
+
+        if len(self.tokens) > 1:
+            values = " ".join(str(t.value) for t in self.tokens[1:])
+            raise ValueError(f"Unexpected items: {values}")
+        return self.eval_token(head, env)
+
+
+# define grammar
+LPAR, RPAR, LBRK, RBRK = (Suppress(c) for c in "()[]")
+number = pyparsing_common.number.setParseAction(Litteral)
+identifier = Word(alphanums, alphanums + "_-.:")
+op = oneOf(" ".join(Operator.op_table))
+bools = oneOf("true false").setParseAction(Bool)
+string_ = QuotedString('"') | QuotedString("'")
+item = (
+    number
+    | op.setParseAction(Operator)
+    | bools
+    | identifier.setParseAction(Identifier)
+    | string_.setParseAction(Litteral)
+)
+
+sexp = Forward()
+sexpList = Group(LPAR + sexp[...] + RPAR)
+sexp <<= item | sexpList
---
+++ tests/sexpr_test.py
@@ -0,0 +1,71 @@
+from numpy import asarray
+
+from lakota.sexpr import AST
+
+trueish_expr = [
+    "(true)",
+    "(~ false)",
+    "(= (- (+ 1 1) (+ 2 2)) -2)",
+    "(= (* (/ 3 2) (/ 7 2)) 5.25)",
+    "(= (/ 6 3 2) 1)",
+    "(or false true false)",
+    "(~ (and false true false))",
+    '(in "foo" "ham" "foo" "bar")',
+]
+
+
+def test_trueish_expr():
+    for code in trueish_expr:
+        ast = AST.parse(code)
+        res = ast.eval()
+        assert res
+
+
+def test_numpy_fun():
+    res = AST.parse("(asarray (list 1 2 3))").eval()
+    assert all(res == asarray([1, 2, 3]))
+
+    res = AST.parse("(max (list 1 2 3))").eval()
+    assert res == 3
+
+    arr = asarray([1, 2, 1, 2])
+    res = AST.parse("(unique arr true)").eval({"arr": arr})
+    assert all(res[0] == [1, 2])
+    assert all(res[1] == [0, 1])
+
+
+def test_some_expr_with_env():
+    env = {
+        "a": asarray([1, 1]),
+        "b": asarray([2, 2]),
+        "x-x": 1,
+        "y_y": 2,
+        "frame": {
+            "u": 1,
+            "v": 2,
+        },
+    }
+    res = AST.parse("(+ a b)").eval(env)
+    expected = asarray([3, 3])
+    assert all(res == expected)
+
+    ast = AST.parse("(+ x-x y_y)")
+    assert ast.eval(env) == 3
+
+    ast = AST.parse("(+ frame.u frame.v)")
+    assert ast.eval(env) == 3
+
+
+def test_only_litterals():
+    res = AST.parse("(list 1 2 3)").eval()
+    assert res == [1, 2, 3]
+
+    res = AST.parse("(1)").eval()
+    assert res == 1
+
+    res = AST.parse('("spam")').eval()
+    assert res == "spam"
+
+    res = AST.parse('(dict "ham" 1 "spam" 2)').eval()
+    assert res["ham"] == 1
+    assert res["spam"] == 2
