7f974a0d5e914b5751870b50c175f8e4c83d2913
Bertrand Chenal <bertrand.chenal@engie.com>
2021-03-22 10:20:29 +0100

    Fix & add script output

--- bench/bench_bisect.py
+++ bench/bench_bisect.py
@@ -1,43 +1,55 @@
+# Script output:
+
+# item 0.011694499757140875
+# bisect 0.018132199998944998
+# searchsorted 1.1717986003495753
+
+
 import os
 from bisect import bisect
-from numpy import arange, searchsorted
-import zarr
-from time import perf_counter
 from contextlib import contextmanager
+from time import perf_counter
+
+import zarr
+from numpy import arange, searchsorted
+
 
 @contextmanager
-def timeit(title=''):
+def timeit(title=""):
     start = perf_counter()
     yield
     delta = perf_counter() - start
     print(title, delta)
 
 
+store = "bisect_test_store.zarr"
+
+
 def load():
-    store = 'bisect_test_store.zarr'
-    if os.exists(store):
+    if os.path.exists(store):
         return
     step = 500_000
-    z = zarr.open(store, mode='w', shape=(0,),
-                  chunks=(10_000), dtype='i8')
+    z = zarr.open(store, mode="w", shape=(0,), chunks=(10_000), dtype="i8")
 
     for i in range(0, 100 * step, step):
         a = arange(i, i + step)
-        with timeit(f'save {i}'):
+        with timeit(f"save {i}"):
             z.append(a)
+
 
 def timings():
     # z = da.from_zarr('example.zarr')
-    z = zarr.open('example.zarr')
+    z = zarr.open(store)
 
-    with timeit('item'):
-        v = z[500]
+    with timeit("item"):
+        _ = z[500]
 
-    with timeit('bisect'):
+    with timeit("bisect"):
         assert 5000001 == bisect(z, 5000000)
 
-    with timeit('searchsorted'):
+    with timeit("searchsorted"):
         assert 5000000 == searchsorted(z, 5000000)
 
-#load()
+
+load()
 timings()
