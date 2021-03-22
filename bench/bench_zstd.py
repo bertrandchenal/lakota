a477b8bfc540410d6a7cae4614268abd95cbb552
Bertrand Chenal <bertrand.chenal@engie.com>
2021-03-22 10:16:21 +0100

    Add script output

--- bench/bench_zstd.py
+++ bench/bench_zstd.py
@@ -1,3 +1,32 @@
+# Script output:
+
+# ** arange
+#   raw 800.00k
+#   blosc 0.00671 5.37k
+#   zstd 0.1277825 102.23k
+#   lz4 0.50026125 400.21k
+# ** random
+#   raw 800.00k
+#   blosc 0.87689625 701.52k
+#   zstd 0.93866375 750.93k
+#   lz4 1.00392875 803.14k
+# ** mixed
+#   raw 800.00k
+#   blosc 0.87322375 698.58k
+#   zstd 0.5925975 474.08k
+#   lz4 0.79945625 639.57k
+# ** random_sorted
+#   raw 800.00k
+#   blosc 0.69136125 553.09k
+#   zstd 0.92005625 736.04k
+#   lz4 1.00392875 803.14k
+# ** mixed_sorted
+#   raw 800.00k
+#   blosc 0.36376 291.01k
+#   zstd 0.5220125 417.61k
+#   lz4 0.7560675 604.85k
+
+
 from itertools import chain
 
 from numcodecs import registry
